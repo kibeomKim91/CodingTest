41c83238582fc9780cc028bd60f3f00791513a9d
Kibeom Kim <rlqja0919@gmail.com>
2023-10-03 21:53:24 +0900

    [Bronze V] Title: A+B - 3, Time: 236 ms, Memory: 17836 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        int A;
+        int B;
+        
+        for(int i=0; i<T; i++) {
+            A = sc.nextInt();
+            B = sc.nextInt();
+            System.out.println(A + B);
+        }
+    }
+    
+}
