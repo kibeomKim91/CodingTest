17c52ebcb3ec542a772d7a642ea9e5fa1c91a822
Kibeom Kim <rlqja0919@gmail.com>
2023-10-13 22:43:44 +0900

    [Bronze IV] Title: Identifying tea, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        int result = 0;
+        
+        for(int i=0; i<5; i++) {
+            result = sc.nextInt();
+            
+            if(n == result) {
+                count++;
+            }
+        }
+        
+        System.out.print(count);
+    }
+    
+}
