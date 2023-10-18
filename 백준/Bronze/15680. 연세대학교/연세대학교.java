5800e1fdc10f3f75b21b1699c2e72a927163ab23
Kibeom Kim <rlqja0919@gmail.com>
2023-10-18 23:28:12 +0900

    [Bronze V] Title: 연세대학교, Time: 208 ms, Memory: 17224 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if(n == 0) {
+            System.out.print("YONSEI");
+        } else {
+            System.out.print("Leading the Way to the Future");
+        }
+    }
+    
+}
