0a291aab4c9019ba3b12a55fcb61cd5c24abf13a
Kibeom Kim <rlqja0919@gmail.com>
2023-10-21 08:29:25 +0900

    [Bronze IV] Title: Easy-to-Solve Expressions, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/25784. Easy－to－Solve Expressions/Easy－to－Solve Expressions.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if(a+b == c || a+c == b || b+c == a) {
+            System.out.print(1);
+        } else if(a*b == c || a*c == b || b*c == a) {
+            System.out.print(2);
+        } else {
+            System.out.print(3);
+        }
+    }
+    
+}
