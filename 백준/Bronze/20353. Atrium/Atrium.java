866a4321b6a29a6be3da593d308765eff5976d4c
Kibeom Kim <rlqja0919@gmail.com>
2023-10-21 08:58:17 +0900

    [Bronze IV] Title: Atrium, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        double d = Math.sqrt(n);
+        
+        System.out.print(4*d);
+    }
+    
+}
