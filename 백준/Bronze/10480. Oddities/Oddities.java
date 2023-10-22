623655bc676c63ab1d5ed69ead16dc174398e727
Kibeom Kim <rlqja0919@gmail.com>
2023-10-22 18:34:32 +0900

    [Bronze IV] Title: Oddities, Time: 228 ms, Memory: 18240 KB -BaekjoonHub

---
+++ 백준/Bronze/10480. Oddities/Oddities.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n; i++) {
+            int num = sc.nextInt();
+            
+            if(num % 2 == 1 || num % 2 == -1) {
+                System.out.println(num + " is odd");
+            } else if(num % 2 == 0) {
+                System.out.println(num + " is even");
+            } else if(num == 0) {
+                System.out.println(num + " is odd");
+            }
+        }
+    }
+    
+}
