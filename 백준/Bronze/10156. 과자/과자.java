0d0ea3a745ec48ca64f2a8573f80ecb6131420ae
Kibeom Kim <rlqja0919@gmail.com>
2023-10-27 21:52:39 +0900

    [Bronze IV] Title: 과자, Time: 216 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if(a*b > c) {
+            System.out.print(a*b-c);
+        } else {
+            System.out.print(0);
+        }
+    }
+    
+}
