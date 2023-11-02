c443a39e6488219f4c4a5af5e0062ca7fcbf67bf
Kibeom Kim <rlqja0919@gmail.com>
2023-11-02 22:27:30 +0900

    [Bronze IV] Title: Koszykarz, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/8710. Koszykarz/Koszykarz.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if((b-a)%c == 0) {
+            System.out.print((b-a)/c);
+        } else {
+            System.out.print((b-a)/c+1);
+        }
+    }
+    
+}
