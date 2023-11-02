6022127b4e378e8c6d8259b63634265c4bd79b6c
Kibeom Kim <rlqja0919@gmail.com>
2023-11-02 22:57:41 +0900

    [Bronze IV] Title: Terms of Office, Time: 228 ms, Memory: 18300 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        while(x <= y) {
+            System.out.println("All positions change in year " + x);
+            x += 60;
+        }
+    }
+    
+}
