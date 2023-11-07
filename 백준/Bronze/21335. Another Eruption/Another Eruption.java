39c23e8aea849a4b1aaa13e27d40890e28c6d359
Kibeom Kim <rlqja0919@gmail.com>
2023-11-07 21:41:46 +0900

    [Bronze IV] Title: Another Eruption, Time: 208 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/21335. Another Eruption/Another Eruption.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        double d = Math.sqrt(n / Math.PI);
+        
+        System.out.print(d*2*Math.PI);
+    }
+    
+}
