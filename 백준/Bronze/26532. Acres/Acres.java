853f2bac435df45beabd2f307855f644dd034144
Kibeom Kim <rlqja0919@gmail.com>
2023-11-07 21:48:34 +0900

    [Bronze IV] Title: Acres, Time: 208 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/26532. Acres/Acres.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        double d = (double) a * b / 4840;
+        
+        if(d%5 == 0) {
+            System.out.print((int) d/5);
+        } else {
+            System.out.print((int) d/5+1);
+        }
+    }
+    
+}
