ac240d863b6ceceb0bddf3e582ad7ca30490564c
Kibeom Kim <rlqja0919@gmail.com>
2023-11-10 21:03:15 +0900

    [Bronze III] Title: Culture shock, Time: 728 ms, Memory: 65072 KB -BaekjoonHub

---
+++ 백준/Bronze/25277. Culture shock/Culture shock.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int count = 0;
+        
+        for(int i=0; i<n; i++) {
+            String str = sc.next();
+            
+            if(str.equals("he") || str.equals("him") || str.equals("she") || str.equals("her")) {
+                count++;
+            }
+        }
+        
+        System.out.print(count);
+    }
+    
+}
