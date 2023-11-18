0b87c934295f50ac51d8f03f49f2834d9ee18894
Kibeom Kim <rlqja0919@gmail.com>
2023-11-18 18:33:21 +0900

    [Bronze III] Title: Divisors, Time: 232 ms, Memory: 18424 KB -BaekjoonHub

---
+++ 백준/Bronze/13225. Divisors/Divisors.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n; i++) {
+            int num = sc.nextInt();
+            int count = 0;
+            
+            for(int j=1; j<=num; j++) {
+                if(num%j == 0) {
+                    count++;
+                }
+            }
+            
+            System.out.println(num+" "+count);
+        }
+    }
+    
+}
