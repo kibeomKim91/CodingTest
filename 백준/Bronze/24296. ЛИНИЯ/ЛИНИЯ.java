0d2b46ed89a546b3de4868297a443b34202c5a2d
Kibeom Kim <rlqja0919@gmail.com>
2023-11-20 23:35:21 +0900

    [Bronze III] Title: ЛИНИЯ, Time: 216 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/24296. ЛИНИЯ/ЛИНИЯ.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        while(true) {
+            if(n%2 == 0) {
+                System.out.print(n);
+                break;
+            } else {
+                n -= n / 2;
+            }
+        }
+    }
+    
+}
