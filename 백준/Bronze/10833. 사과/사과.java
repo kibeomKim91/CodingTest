05d73f491c5c6a14b9a17b0cc06c3ac303bee660
Kibeom Kim <rlqja0919@gmail.com>
2023-11-24 20:34:33 +0900

    [Bronze III] Title: 사과, Time: 232 ms, Memory: 17884 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        
+        for(int i=0; i<n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            sum += b%a;
+        }
+        
+        System.out.print(sum);
+    }
+    
+}
