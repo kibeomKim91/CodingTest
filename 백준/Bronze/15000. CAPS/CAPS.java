25fcb354e19183ad666a414031e75c133c9cec2e
Kibeom Kim <rlqja0919@gmail.com>
2024-01-05 19:26:32 +0900

    [Bronze V] Title: CAPS, Time: 544 ms, Memory: 32040 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String result = str.toUpperCase();
+        
+        System.out.print(result);
+    }
+    
+}
