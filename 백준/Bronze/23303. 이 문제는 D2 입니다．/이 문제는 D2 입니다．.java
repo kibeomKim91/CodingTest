ee36787a0ee4e04084c86e1edc138412b9bcae20
Kibeom Kim <rlqja0919@gmail.com>
2024-01-31 16:19:52 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 248 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+        if(str.contains("d2") || str.contains("D2")) {
+            System.out.print("D2");
+        } else {
+            System.out.print("unrated");
+        }
+    }
+    
+}
