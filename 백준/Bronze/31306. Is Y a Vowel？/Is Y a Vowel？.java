0b255bc24f5c9b545ed9972dd77167c7f0d3427d
Kibeom Kim <rlqja0919@gmail.com>
2024-02-03 17:39:36 +0900

    [Bronze IV] Title: Is Y a Vowel?, Time: 232 ms, Memory: 18364 KB -BaekjoonHub

---
+++ 백준/Bronze/31306. Is Y a Vowel？/Is Y a Vowel？.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int strLength = str.length();
+        int count = 0;
+        int yCount = 0;
+        
+        for(int i=0; i<strLength; i++) {
+            if(str.charAt(i) == 'a' ||
+              str.charAt(i) == 'e' ||
+              str.charAt(i) == 'i' ||
+              str.charAt(i) == 'o' ||
+              str.charAt(i) == 'u') {
+                count++;
+            } else if(str.charAt(i) == 'y') {
+                yCount++;
+            }
+        }
+        
+        System.out.print(count+" "+(count+yCount));
+    }
+    
+}
