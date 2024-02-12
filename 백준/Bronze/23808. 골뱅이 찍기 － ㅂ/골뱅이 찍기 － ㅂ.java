368cd8eef59641f00be16ed26daf335d0c036ad6
Kibeom Kim <rlqja0919@gmail.com>
2024-02-12 17:11:25 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 1064 ms, Memory: 27504 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n*2; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.print("@");
+            }
+            
+            for(int j=0; j<n; j++) {
+                System.out.print("   ");
+            }
+            
+            for(int j=0; j<n; j++) {
+                System.out.print("@");
+            }
+            
+            System.out.println("");
+        }
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.print("@@@@@");
+            }
+            
+            System.out.println("");
+        }
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.print("@");
+            }
+            
+            for(int j=0; j<n; j++) {
+                System.out.print("   ");
+            }
+            
+            for(int j=0; j<n; j++) {
+                System.out.print("@");
+            }
+            
+            System.out.println("");
+        }
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.print("@@@@@");
+            }
+            
+            System.out.println("");
+        }
+    }
+    
+}
