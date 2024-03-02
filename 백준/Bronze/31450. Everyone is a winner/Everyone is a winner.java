43254d60ef5186fe58aac18c7f2aec4e3324da9d
Kibeom Kim <rlqja0919@gmail.com>
2024-03-02 22:52:19 +0900

    [Bronze V] Title: Everyone is a winner, Time: 200 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        if(n%k == 0) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+    }
+    
+}
