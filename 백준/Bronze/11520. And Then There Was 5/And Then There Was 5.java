3d1b3d14f7bb8472bc1c5953d436c2b7f6be8d2d
Kibeom Kim <rlqja0919@gmail.com>
2024-04-08 19:47:26 +0900

    [Bronze III] Title: And Then There Was 5, Time: 784 ms, Memory: 45584 KB -BaekjoonHub

---
+++ 백준/Bronze/11520. And Then There Was 5/And Then There Was 5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n; i++) {
+            int p = sc.nextInt();
+            int d = sc.nextInt();
+            
+            System.out.println(d+" "+5);
+        }
+    }
+    
+}
