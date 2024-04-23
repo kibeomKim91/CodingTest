5038b523e02e636fa33aaa9f292beffbaabf5f41
Kibeom Kim <rlqja0919@gmail.com>
2024-04-23 19:11:00 +0900

    [Bronze III] Title: Shifty Sum, Time: 204 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/14682. Shifty Sum/Shifty Sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int result = n;
+        
+        for(int i=0; i<k; i++) {
+            n *= 10;
+            result += n;
+        }
+        
+        System.out.print(result);
+    }
+    
+}
