ba44bcc1199b88aeac7d6b7772c5ec3eff8d73b4
Kibeom Kim <rlqja0919@gmail.com>
2024-06-07 20:11:33 +0900

    [Bronze II] Title: 점수 집계, Time: 200 ms, Memory: 17488 KB -BaekjoonHub

---
+++ 백준/Bronze/9076. 점수 집계/점수 집계.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n; i++) {
+            int[] arr = new int[5];
+            
+            for(int j=0; j<5; j++) {
+                arr[j] = sc.nextInt();
+            }
+            
+            Arrays.sort(arr);
+            if(arr[3]-arr[1]>=4) {
+                System.out.println("KIN");
+            } else {
+                System.out.println(arr[1]+arr[2]+arr[3]);
+            }
+        }
+    }
+    
+}
