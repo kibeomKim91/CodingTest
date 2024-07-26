fda52f70d94743950cd4b6f0d92e83b923826f1e
Kibeom Kim <rlqja0919@gmail.com>
2024-07-26 20:58:59 +0900

    [Bronze III] Title: XMAS, Time: 696 ms, Memory: 40304 KB -BaekjoonHub

---
+++ 백준/Bronze/9838. XMAS/XMAS.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i=0; i<n; i++) {
+            int num = sc.nextInt()-1;
+            arr[num] = i+1;
+        }
+        
+        for(int i=0; i<n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
